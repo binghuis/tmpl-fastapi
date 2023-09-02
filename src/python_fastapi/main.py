f904a6cf0e06afc85a97dd55c66a27ff032b8c9a
songbinghui <soongbinghui@gmail.com>
2023-09-02 18:56:05 +0800

    feat: cors

--- src/python_fastapi/main.py
+++ src/python_fastapi/main.py
@@ -1,27 +1,34 @@
-from typing import Union
-
-from fastapi import FastAPI
+from enum import Enum
+from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(title="Fastapi Demo")
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origin_regex="",
+    expose_headers=[],
+    max_age=600,
+)
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "Hello World"
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+async def read_items(
+    item_id: Annotated[int, Path(description="通过 id 获取 item")],
+    q: Annotated[str, Query(alias="item-query", description="查询参数 q")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
