a29b1293da848fd51ec7bb18d8da5fecc5bdb761
songbinghui <soongbinghui@gmail.com>
2023-09-04 12:10:51 +0800

    feat: demo

--- src/python_fastapi/main.py
+++ src/python_fastapi/main.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Path, Query
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="Fastapi Demo")
 
@@ -23,12 +23,39 @@ def read_root():
     return "Hello World"
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    item_id: Annotated[int, Path(description="通过 id 获取 item")],
-    q: Annotated[str, Query(alias="item-query", description="查询参数 q")] = None,
+@app.get("/users")
+async def read_users(
+    *,
+    page_size: Annotated[int, Query(description="每页条数", ge=10)] = 10,
+    page_index: Annotated[int, Query(description="索引", gt=1)] = 1,
 ):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+    results = {"page_index": page_index, "page_size": page_size}
     return results
+
+
+@app.get("/users/{id}")
+async def read_user(id: Annotated[int, Path(description="用户 ID")]):
+    return {"id": id}
+
+
+class UserDetail(BaseModel):
+    address: str = None
+
+
+class User(BaseModel):
+    name: str = Field(examples=["李华"])
+    age: int = Field(examples=[16])
+    detail: UserDetail = Field()
+    model_config = {
+        "json_schema_extra": {"examples": [{name: "李华", age: 16, detail: {}}]}
+    }
+
+
+@app.post("/users/create")
+async def create_user(user: User):
+    return user
+
+
+@app.put("/users/{id}")
+async def update_user():
+    pass
